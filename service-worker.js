a7edc2d8312e4764359a71da80726191ce8188f9
rinsaathi-advisory <rinsaathiadvisory@gmail.com>
2026-09-13 13:02:11 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener(
+  "install",
+  function(){
+    self.skipWaiting();
+  }
+);
+
+self.addEventListener(
+  "activate",
+  function(event){
+    event.waitUntil(
+      self.clients.claim()
+    );
+  }
+);
+
+self.addEventListener(
+  "fetch",
+  function(){
+  }
+);
